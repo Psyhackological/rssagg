2995687ae09c89fd03930fa11498f620b26155a5
Psyhackological <konradkon@duck.com>
2025-05-13 19:16:26 +0200

    Tidy up everything

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Psyhackological/7-go-projects
 
 go 1.24.2
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load(".env")
 	portString := os.Getenv("PORT")
+
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	fmt.Println("Port: ", portString)
+	fmt.Println("Port:", portString)
 }
