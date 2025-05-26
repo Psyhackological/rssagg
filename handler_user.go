d88106981a3445c2afef3a5213a44f31131a24e2
Psyhackological <konradkon@duck.com>
2025-05-26 18:25:52 +0200

    Add handlerGetUser

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Psyhackological/rssagg/internal/auth"
 	"github.com/Psyhackological/rssagg/internal/database"
 	"github.com/google/uuid"
 )
@@ -33,5 +34,21 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
 		return
 	}
-	respondWithJSON(w, 200, user)
+	respondWithJSON(w, 201, databaseUserTouser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databaseUserTouser(user))
 }
