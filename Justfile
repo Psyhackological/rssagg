6d4d52237612b3ec1e273a49f18430dee0b9e8e7
Psyhackological <konradkon@duck.com>
2025-05-26 17:45:43 +0200

    Add SQL handling with POST users table

--- Justfile
+++ Justfile
@@ -1,4 +1,50 @@
 alias r := run
+alias cr := container_run
 
 run:
   go build && ./rssagg
+
+# Get a Go package and clean up modules
+goget package:
+    go get {{package}}
+    go mod tidy
+    go mod vendor
+
+# Load environment variables from .env file
+set dotenv-load
+
+# Container runtime (docker or podman)
+runtime := env_var_or_default("CONTAINER_RUNTIME", "podman")
+postgres_container := env_var_or_default("POSTGRES_CONTAINER", "my-postgres")
+postgres_port := env_var_or_default("POSTGRES_PORT", "5432")
+postgres_volume := env_var_or_default("POSTGRES_VOLUME", "postgres_data")
+
+container_run:
+    {{runtime}} run -d \
+        --name {{postgres_container}} \
+        --env-file .env \
+        -p {{postgres_port}}:5432 \
+        -v {{postgres_volume}}:/var/lib/postgresql/data \
+        docker.io/library/postgres:15
+
+# Stop the container
+stop:
+    {{runtime}} stop {{postgres_container}}
+
+# Force use Docker
+docker-run:
+    docker run -d \
+        --name {{postgres_container}} \
+        --env-file .env \
+        -p {{postgres_port}}:5432 \
+        -v {{postgres_volume}}:/var/lib/postgresql/data \
+        docker.io/library/postgres:15
+
+# Force use Podman
+podman-run:
+    podman run -d \
+        --name {{postgres_container}} \
+        --env-file .env \
+        -p {{postgres_port}}:5432 \
+        -v {{postgres_volume}}:/var/lib/postgresql/data \
+        docker.io/library/postgres:15
--- main.go
+++ main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Psyhackological/rssagg/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
-	portString := os.Getenv("PORT")
-
-	if portString == "" {
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
-	fmt.Println("Port:", portString)
+	fmt.Println("Port:", PORT)
+
+	DB_URL := os.Getenv("DB_URL")
+	if DB_URL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", DB_URL)
+	if err != nil {
+		log.Fatal("Can't connect to database")
+	}
+
+	queries := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: queries,
+	}
 
 	router := chi.NewRouter()
 
@@ -34,15 +57,16 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
-		Addr:    ":" + portString,
+		Addr:    ":" + PORT,
 	}
 
-	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
+	log.Printf("Server starting on port %v", PORT)
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
